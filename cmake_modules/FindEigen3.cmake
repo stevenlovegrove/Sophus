ada1503f624256802e8809646d22cc33def60471
solosuper <ruffens@gmail.com>
2016-05-09 12:34:02 +0200

    update FindEigen3.cmake, look for env var EIGEN3_INCLUDE_DIR.

--- cmake_modules/FindEigen3.cmake
+++ cmake_modules/FindEigen3.cmake
@@ -67,6 +67,7 @@ else (EIGEN3_INCLUDE_DIR)
       /opt/local/include
       ${CMAKE_INSTALL_PREFIX}/include
       ${KDE4_INCLUDE_DIR}
+	  $ENV{EIGEN3_INCLUDE_DIR}
       PATH_SUFFIXES eigen3 eigen
     )
 
